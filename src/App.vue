673257ddb20b0a075ea892d60f340b3efabfa3ed
Luiz Filho <luiz.filho@mutantbr.com>
2019-09-25 22:33:31 -0300

    sufixes and prefixes before v-for

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,45 @@
 <template>
   <div>
-    <h1 class="text-center"><span class="fa fa-check">Dinossaurus</span></h1>
+      <div id="slogan" class="text-center">
+        <h1><span class="fa fa-check">Dinossaurus</span></h1>
+        <hr>
+        <h6 class="text-secondary">Gerador de nomes utlizando Vue.js</h6>
+    </div>
+    <div id="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <h5>Prefixos <span class="badge badge-info">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item">A</li>
+                  <li class="list-group-item">B</li>
+                  <li class="list-group-item">C</li>
+                </ul>
+                <br>
+                <input class="form-control" type="text" placeholder="Digite o prefixo">
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <h5>Sufixos <span class="badge badge-info">0</span></h5>
+            <div class="card">
+              <div class="card-body">
+                <ul class="list-group">
+                  <li class="list-group-item">A</li>
+                  <li class="list-group-item">B</li>
+                  <li class="list-group-item">C</li>
+                </ul>
+                <br>
+                <input class="form-control" type="text" placeholder="Digite o sufixo">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
@@ -8,10 +47,23 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'font-awesome/css/font-awesome.css'
 export default {
-  name: 'app'
+  name: 'app',
+  data: function () {
+    return {
+      prefixes: ['Air', 'Jet', 'Flight'],
+      sufixes: ['Sufixes', 'Station', 'Mart']
+    }
+  }
 }
 </script>
-
 <style>
-
+  #slogan {
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+  #main {
+    background-color: #f1f1f1;
+    padding-top: 30px;
+    padding-bottom: 30px;
+  }
 </style>
